549eae43bacb336a259d98a0cf1eb0b8e62f8969
Billy <billywelch@gmail.com>
2014-07-25 18:42:31 -0700

    version bump

--- bin/versionInfo.dart
+++ bin/versionInfo.dart
@@ -1,6 +1,6 @@
 class VersionInfo{
    static String _name = "Spokes";
-   static String _version = "0.2.0";
+   static String _version = "0.3.0";
    static String _codeName = "Raleigh";
 
    static String get version => "${_name} ${_version} (${_codeName})";
